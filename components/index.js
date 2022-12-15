ff9ace86534601a29d4ab3c6f55b17a7aa774f43
Anurag Mishra <anuragtej14@gmail.com>
2022-12-15 10:59:18 +0530

    modefied code

---
+++ components/index.js
@@ -0,0 +1,28 @@
+import Navbar from "./Navbar";
+import About from "./About";
+import Contact from "./Contact";
+import Faq from "./Faq";
+import FaqElement from "./FaqElement";
+import Footer from "./Footer";
+import Graph from "./Graph";
+import Hero from "./Hero";
+import MenuDropDown from "./MenuDropDown";
+import Mountains from "./Mountains";
+import Team  from  "./Team";
+import Trade from "./Trade";
+
+
+export {
+    Navbar,
+    About,
+    Contact,
+    Faq,
+    FaqElement,
+    Footer,
+    Graph,
+    Hero,
+    MenuDropDown,
+    Mountains,
+    Team,
+    Trade
+}
